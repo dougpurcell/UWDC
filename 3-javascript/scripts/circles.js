bab55668c26025215a604a2ff55f261119db5b51
Doug <griffithsdl302@gmail.com>
2019-12-30 16:49:06 -0500

    circles

---
+++ 3-javascript/scripts/circles.js
@@ -0,0 +1,9 @@
+document.getElementById("redCircle").onclick = function (){
+    document.getElementById("redCircle").style.display = "none";
+};
+document.getElementById("blueCircle").onclick = function (){
+    document.getElementById("blueCircle").style.display = "none";
+};
+document.getElementById("yellowCircle").onclick = function (){
+    document.getElementById("yellowCircle").style.display = "none";
+};
